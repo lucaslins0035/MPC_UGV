a969c84fd298420bda7b7c3aaff393f659d217f8
lucaslins0035 <lucaslinssouza@gmail.com>
2023-04-13 21:07:55 -0300

    Add costmap obs avoidance

--- gem_path_tracking/scripts/path_tracking_server.py
+++ gem_path_tracking/scripts/path_tracking_server.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import threading
+import uuid
 import rospy
 import actionlib
 import do_mpc
@@ -19,6 +20,7 @@ from gem_interfaces.msg import TrackPathAction, TrackPathResult
 class TrackPathServer:
     def __init__(self):
         self.init_tf = None
+        # self.center_footprint_tf = None
         self.stop_getting_tf = False
         self.path_tracking_error = []
         self.path_index = 0
@@ -37,6 +39,7 @@ class TrackPathServer:
         self.costmap = None
         self.costmap_res = None
         self.costmap_size = None
+        self.costmap_size_px = None
         self.costmap_pub = rospy.Subscriber(
             '/costmap_node/costmap/costmap', OccupancyGrid, self.costmap_cb)
 
@@ -47,31 +50,38 @@ class TrackPathServer:
         rospy.sleep(2.0)
         rospy.loginfo("Server initialized")
 
-    def get_cell_cost(self, x, y):
-        diff_x = x - self.costmap_orig[0]
-        diff_y = y - self.costmap_orig[1]
+    # def get_cell_cost(self, x, y):
+    #     diff_x = x - self.costmap_orig[0]
+    #     diff_y = y - self.costmap_orig[1]
 
-        if diff_x < self.costmap_size[0] and \
-                diff_y < self.costmap_size[1]:
-            return self.costmap[int((self.costmap_size[1]-diff_y)/self.costmap_res),
-                                int(diff_x/self.costmap_res)]
+    #     if diff_x < self.costmap_size[0] and \
+    #             diff_y < self.costmap_size[1]:
+    #         return float(self.costmap[int((self.costmap_size[1]-diff_y)/self.costmap_res),
+    #                                   int(diff_x/self.costmap_res)])
 
-        return 0.0
+    #     return 0.0
+
+    def get_cell_position(self, row, col):
+        return [(col*self.costmap_res + self.costmap_orig[0],
+                 (self.costmap_size[1] - row*self.costmap_res) + self.costmap_orig[1])]
 
     def costmap_cb(self, msg):
         if self.costmap is None:
             self.costmap_res = msg.info.resolution
-            self.costmap_size = (msg.info.width*self.costmap_res,
-                                 msg.info.height*self.costmap_res)
-            
+            self.costmap_size = (msg.info.width*self.costmap_res/2,
+                                 msg.info.height*self.costmap_res)  # Ref: (x,y) ROS
+
+            self.costmap_size_px = (msg.info.height,
+                                    msg.info.width//2)  # Ref: (rows, cols) array
+
             rospy.loginfo("Res "+str(self.costmap_res))
             rospy.loginfo("Size "+str(self.costmap_size))
-        
+
         rospy.loginfo("Update")
-        
+
         self.costmap = np.array(msg.data).reshape(msg.info.height,
-                                                  msg.info.width)
-        self.costmap_orig = [msg.info.origin.position.x,
+                                                  msg.info.width)[:,self.costmap_size_px[1]:]
+        self.costmap_orig = [msg.info.origin.position.x + self.costmap_size[0],
                              msg.info.origin.position.y]
 
     def get_tf(self):
@@ -92,6 +102,22 @@ class TrackPathServer:
                                            trans.transform.rotation.y,
                                            trans.transform.rotation.z,
                                            trans.transform.rotation.w))[2]]).reshape(-1, 1)
+
+            # try:
+            #     trans = self.tfBuffer.lookup_transform(
+            #         'odom', 'center_footprint', rospy.Time())
+            # except:
+            #     rospy.logwarn_throttle(
+            #         2.0,
+            #         "Failed to find TF from 'odom' to 'center_footprint'")
+            # else:
+            #     self.center_footprint_tf = np.array([
+            #         trans.transform.translation.x,
+            #         trans.transform.translation.y,
+            #         euler_from_quaternion((trans.transform.rotation.x,
+            #                                trans.transform.rotation.y,
+            #                                trans.transform.rotation.z,
+            #                                trans.transform.rotation.w))[2]]).reshape(-1, 1)
             tf_rate.sleep()
 
     def setup_controller(self, action_goal):
@@ -125,12 +151,23 @@ class TrackPathServer:
             var_type='_tvp', var_name='x_set', shape=(1, 1))
         self.tvp_y_set = self.model.set_variable(
             var_type='_tvp', var_name='y_set', shape=(1, 1))
+        self.tvp_costmap = self.model.set_variable(
+            var_type='_tvp', var_name='costmap', shape=(self.costmap_size_px[0],
+                                                        self.costmap_size_px[1]))
+        self.tvp_cell_pos_x = self.model.set_variable(
+            var_type='_tvp', var_name='cell_pos_x', shape=(self.costmap_size_px[0],
+                                                           self.costmap_size_px[1]))
+        self.tvp_cell_pos_y = self.model.set_variable(
+            var_type='_tvp', var_name='cell_pos_y', shape=(self.costmap_size_px[0],
+                                                           self.costmap_size_px[1]))
 
         # Set right-hand side
-        self.model.set_rhs('x', self.input_v*cdi.cos(self.state_theta))
-        self.model.set_rhs('y', self.input_v*cdi.sin(self.state_theta))
-        self.model.set_rhs('theta', self.input_v *
-                           cdi.tan(self.input_psi)/self.param_l)
+        self.model.set_rhs(
+            'x', self.input_v*cdi.cos(self.state_theta + cdi.atan(cdi.tan(self.input_psi)/2)))
+        self.model.set_rhs(
+            'y', self.input_v*cdi.sin(self.state_theta + cdi.atan(cdi.tan(self.input_psi)/2)))
+        self.model.set_rhs('theta', self.input_v*cdi.tan(self.input_psi)
+                           * cdi.cos(cdi.atan(cdi.tan(self.input_psi)/2)))
         # self.model.set_rhs('psi', psi_dot)
 
         self.model.setup()
@@ -144,15 +181,17 @@ class TrackPathServer:
         n_horizon = 20
         setup_mpc = {
             'n_horizon': n_horizon,
-            't_step': 0.2,
+            't_step': 0.5,
             'store_full_solution': True,
             'nlpsol_opts': {'ipopt.print_level': 0, 'ipopt.sb': 'yes', 'print_time': 0}
         }
         self.mpc.set_param(**setup_mpc)
 
-        # Objective function
+        # cdi.Function('costmap_cost', [self.state_x, self.state_y],)
+
+        # Objective function #TODO? + 1*(theta - theta_set)**2
         lterm = 1e-2 * (self.state_x - self.tvp_x_set)**2 + 1e-2 * \
-            (self.state_y - self.tvp_y_set)**2  # + 1*(theta - 0)**2
+            (self.state_y - self.tvp_y_set)**2
         mterm = lterm
 
         self.mpc.set_objective(mterm=mterm, lterm=lterm)
@@ -162,13 +201,14 @@ class TrackPathServer:
             psi=1e0)
 
         # Parameters
-        p_template = self.mpc.get_p_template(1)
-        p_template['_p', 0] = np.array([1.75, 0.82])
+        self.mpc.set_uncertainty_values(l=[1.75], car_radius=[1.5])
+        # p_template = self.mpc.get_p_template(1)
+        # p_template['_p', 0] = np.array([1.75, 0.82])
 
-        def p(t_now):
-            return p_template
+        # def p(t_now):
+        #     return p_template
 
-        self.mpc.set_p_fun(p)
+        # self.mpc.set_p_fun(p)
 
         # Time-varying Parameters
         tvp_template = self.mpc.get_tvp_template()
@@ -179,15 +219,32 @@ class TrackPathServer:
             pose.pose.position.y for pose in action_goal.path.poses]
 
         def tvp_fun(t_now):
-            for k in range(n_horizon+1):
-                tvp_template['_tvp', k,
-                             'x_set'] = self.path_x_cp[self.path_index]
-                tvp_template['_tvp', k,
-                             'y_set'] = self.path_y_cp[self.path_index]
+            tvp_template['_tvp', :,
+                         'x_set'] = self.path_x_cp[self.path_index]
+            tvp_template['_tvp', :,
+                         'y_set'] = self.path_y_cp[self.path_index]
+
+
+            tvp_template['_tvp', :,
+                         'costmap'] = self.costmap
+
+
+            cell_pos = np.zeros(
+                (self.costmap_size_px[0], self.costmap_size_px[1], 2), dtype=np.float32)
+            for i in np.arange(self.costmap_size_px[0]):
+                for j in np.arange(self.costmap_size_px[1]):
+                    cell_pos[i, j] = np.array(self.get_cell_position(i, j))
+
+
+            tvp_template['_tvp', :, 'cell_pos_x'] = cell_pos[:, :, 0]
+            tvp_template['_tvp', :, 'cell_pos_y'] = cell_pos[:, :, 1]
+
 
             return tvp_template
 
+
         self.mpc.set_tvp_fun(tvp_fun)
+
 
         # Constraints
 
@@ -215,16 +272,23 @@ class TrackPathServer:
         # mpc.bounds['lower', '_u', 'psi_dot'] = -10.0  # rad/s
 
         # NL Constraints
+        #TODO WAY TO EXPENSIVE!!!
+        for i in np.arange(1, self.costmap_size_px[0]-1):
+            for j in np.arange(1, self.costmap_size_px[1]-1):
+                self.mpc.set_nl_cons(
+                    str(uuid.uuid4()),
+                    cdi.if_else(cdi.logic_and(self.tvp_costmap[i, j] > 90,
+                                              cdi.logic_not(
+                        cdi.logic_and(self.tvp_costmap[i-1, j] > 90,
+                                      cdi.logic_and(self.tvp_costmap[i+1, j] > 90,
+                                                    cdi.logic_and(self.tvp_costmap[i, j-1] > 90,
+                                                                  self.tvp_costmap[i, j+1] > 90))))),
+                                1.5**2 - (self.state_x-self.tvp_cell_pos_x[i, j])**2 - (
+                        self.state_y-self.tvp_cell_pos_y[i, j])**2,
+                        cdi.SX(0.0)),
+                    ub=0.0,
+                    soft_constraint=True)
 
-        # circles = [
-        #     [1.0, 1.8, 0.2],
-        #     [5.0, 3.4, 0.2],
-        #     # [5.1, 5.5, 0.1],
-        # ]
-
-        # for i, circle in enumerate(circles):
-        #     self.mpc.set_nl_cons('obstacle'+str(i), (self.param_car_radius+circle[2]) - cdi.sqrt(
-        #         (self.state_x-circle[0])**2 + (self.state_y-circle[1])**2), ub=0.0, soft_constraint=True)
 
         self.mpc.setup()
         rospy.loginfo("MPC setup finish successfully!")
@@ -236,9 +300,14 @@ class TrackPathServer:
         self.stop_getting_tf = False
         self.tf_thread.start()
 
+        while self.costmap is None:
+            rospy.logwarn_throttle(
+                2.0, "Waiting for a costmap msg.")
+
         try:
             self.setup_controller(goal)
-        except:
+        except Exception as e:
+            rospy.logerr(str(e))
             rospy.logerr("Failed setting up Controller! Aborting...")
             self.server.set_aborted(TrackPathResult(result=1))
             self.stop_getting_tf = True
@@ -286,6 +355,8 @@ class TrackPathServer:
             u0 = self.mpc.make_step(x0)
             rospy.loginfo("MPC time: {} ms".format(
                 (rospy.get_rostime() - t).to_sec()*1000))
+            
+            rospy.loginfo("Control {} {}".format(u0[0], u0[1]))
 
             # Publish control effort
             control_msg = AckermannDrive()
@@ -314,7 +385,7 @@ class TrackPathServer:
             preds_x = self.mpc.data.prediction(('_x', 'x'))[0, :, 0]
             preds_y = self.mpc.data.prediction(('_x', 'y'))[0, :, 0]
             local_plan.poses.clear()
-            for i in range(len(preds_x)):
+            for i in np.arange(len(preds_x)):
                 pose = PoseStamped()
                 pose.pose.position.x = preds_x[i]
                 pose.pose.position.y = preds_y[i]
